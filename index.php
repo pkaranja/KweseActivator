b1d9e880e4ee86fef321b50ada76399f828d90b9
pkaranja <pittskay@gmail.com>
2018-07-05 12:45:02 +0300

    Login page initial html commit

---
+++ index.php
@@ -0,0 +1,44 @@
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="Ptah Karanja">
+    <link rel="icon" href="favicon.ico">
+
+    <title>Kwese Activator</title>
+
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="css/template.css" rel="stylesheet" />
+
+    <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+  </head>
+
+  <body>
+    <form class="form-signin">
+      <div class="text-center mb-4">
+        <img class="mb-4" src="images/logo_new.png" width="300px">
+      </div>
+
+      <div class="form-label-group">
+        <input type="text" id="inputName" class="form-control" placeholder="Enter full name" required autofocus>
+        <label for="inputName">Full name</label>
+      </div>
+
+      <div class="form-label-group">
+        <input type="text" id="inputPhone" class="form-control" placeholder="Enter phone number" required autofocus>
+        <label for="inputEmail">Phone number</label>
+      </div>
+
+      <div class="form-label-group">
+        <input type="email" id="inputEmail" class="form-control" placeholder="Enter email address" required autofocus>
+        <label for="inputEmail">Email address</label>
+      </div>
+
+      <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+      <p class="mt-5 mb-3 text-muted text-center">&copy; <?php echo date('Y'); ?></p>
+    </form>
+  </body>
+</html>
